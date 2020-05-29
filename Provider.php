9b8c4bd34d872361fedc2dea222718990c8ee7e5
Chamara Abesinghe <12880059+Super-Chama@users.noreply.github.com>
2020-05-29 11:00:57 +0530

    init commit

---
+++ Provider.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace SocialiteProviders\Cognito;
+
+use Illuminate\Support\Arr;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'COGNITO';
+
+    /**
+     * Scopes defintions.
+     *
+     * @see nada
+     */
+    const SCOPE_OPENID = 'openid';
+    const SCOPE_PROFILE = 'profile';
+    const SCOPE_EMAIL = 'email';
+    const SCOPE_ADDRESS = 'address';
+    const SCOPE_PHONE = 'phone';
+    const SCOPE_OFFLINE_ACCESS = 'offline_access';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [
+        'openid',
+        'profile',
+        'email',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCognitoUrl()
+    {
+        return $this->getConfig('base_url');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['base_url'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getCognitoUrl().'/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getCognitoUrl().'/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getCognitoUrl().'/oauth2/userInfo', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'             => Arr::get($user, 'sub'),
+            'email'          => Arr::get($user, 'email'),
+            'email_verified' => Arr::get($user, 'email_verified', false),
+            'nickname'       => Arr::get($user, 'nickname'),
+            'name'           => Arr::get($user, 'name'),
+            'first_name'     => Arr::get($user, 'given_name'),
+            'last_name'      => Arr::get($user, 'family_name'),
+            'profileUrl'     => Arr::get($user, 'profile'),
+            'address'        => Arr::get($user, 'address'),
+            'phone'          => Arr::get($user, 'phone'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
